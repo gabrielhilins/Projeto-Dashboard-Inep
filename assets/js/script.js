91801d2b7afdda4ee67870c0a8196f92bcdf41f7
Gabriel Henrique Lins <130785312+gabrielhilins@users.noreply.github.com>
2023-12-05 00:14:39 -0300

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,90 @@
 async function iniciar() {
   try {
-    // Dados padrão ou outros dados iniciais
-    const dadosPadrao = await obterDados(`/dados`);
-
-    // Chamar a função para atualizar os gráficos com os dados iniciais
-    atualizarGraficos(dadosPadrao);
+    const dadosPadrao = await obterTotaisBasico(`/dados`);
+    inserirGraficoTotalBasico(dadosPadrao);
   } catch (error) {
     console.error('Erro ao iniciar:', error);
   }
 }
 // Chame a função de inicialização
 iniciar();
+
+async function inserirGraficoTotalBasico(dadosPadrao) {
+  try {
+    // Gráfico de Pizza
+    new Chart(document.getElementById('grafico-pizza').getContext('2d'), {
+      type: 'pie',
+      data: {
+        labels: dadosPadrao.map(item => item.regiao),
+        datasets: [{
+          label: `Quantidade de matriculados ${filtroEspecifico} por Região`,
+          data: dadosPadrao.map(item => Number(item.total)),
+          backgroundColor: ['#00BF63', '#FF914D', '#8D723D', '#A6A6A6', '#0047FF']
+        }]
+      }
+    });
+
+    // Gráfico de Linha
+    new Chart(document.getElementById('grafico-linha').getContext('2d'), {
+      type: 'line',
+      data: {
+        labels: dadosPadrao.map(item => item.regiao),
+        datasets: [{
+          label: 'Quantidade de matriculados por Região',
+          borderColor: '#ff6384',
+          data: dadosPadrao.map(item => Number(item.total))
+        }]
+      }
+    });
+
+    // Gráfico de Barra
+    new Chart(document.getElementById('grafico-barra').getContext('2d'), {
+      type: 'bar',
+      data: {
+        labels: dadosPadrao.map(item => item.regiao),
+        datasets: [{
+          label: 'Quantidade de matriculados por Região',
+          backgroundColor: ['#00BF63', '#FF914D', '#8D723D', '#A6A6A6', '#0047FF'],
+          data: dadosPadrao.map(item => Number(item.total))
+        }]
+      }
+    });
+
+    // Gráfico de Barra Horizontal
+    new Chart(document.getElementById('grafico-horizontal-bar').getContext('2d'), {
+      type: 'bar',
+      data: {
+        labels: dadosPadrao.map(item => item.regiao),
+        datasets: [{
+          label: 'Quantidade de matriculados por Região',
+          backgroundColor: ['#00BF63', '#FF914D', '#8D723D', '#A6A6A6', '#0047FF'],
+          data: dadosPadrao.map(item => Number(item.total))
+        }]
+      },
+      options: {
+        indexAxis: 'y',
+        elements: {
+          bar: {
+            borderWidth: 2,
+          }
+        },
+        responsive: true,
+        plugins: {
+          legend: {
+            position: 'bottom',
+          },
+          title: {
+            display: true,
+            text: 'Chart.js Vertical Bar Chart'
+          }
+        }
+      }
+    });
+  } catch (error) {
+    console.error('Erro ao atualizar os gráficos:', error);
+  }
+}
+
 
 async function aplicarFiltros() {
   try {
@@ -31,9 +105,8 @@ async function aplicarFiltros() {
       default:
         break;
     }
-
-    // Obter os dados específicos com base nas opções selecionadas
-    const dadosEspecificos = await obterDados(`/dados/${filtroSelecionado}/${filtroEspecifico}`);
+    const nomeFuncao =  `obterTotais${filtroEspecifico}`;
+    const dadosEspecificos = await nomeFuncao(`/dados/${filtroSelecionado}/${filtroEspecifico}`);
     // Atualizar os gráficos com os dados específicos
     atualizarGraficos(dadosEspecificos);
   } catch (error) {
@@ -41,16 +114,16 @@ async function aplicarFiltros() {
   }
 }
 
-function atualizarGraficos(dados) {
+function atualizarGraficos(dadosEspecificos) {
   try {
     // Gráfico de Pizza
     new Chart(document.getElementById('grafico-pizza').getContext('2d'), {
       type: 'pie',
       data: {
-        labels: dados.map(item => item.regiao),
+        labels: dadosEspecificos.map(item => item.regiao),
         datasets: [{
-          label: 'Quantidade de matriculados por Região',
-          data: dados.map(item => item.total),
+          label: `Quantidade de matriculados ${filtroEspecifico} por Região`,
+          data: dadosEspecificos.map(item => Number(item.total)),
           backgroundColor: ['#00BF63', '#FF914D', '#8D723D', '#A6A6A6', '#0047FF']
         }]
       }
@@ -60,11 +133,11 @@ function atualizarGraficos(dados) {
     new Chart(document.getElementById('grafico-linha').getContext('2d'), {
       type: 'line',
       data: {
-        labels: dados.map(item => item.regiao),
+        labels: dadosEspecificos.map(item => item.regiao),
         datasets: [{
           label: 'Quantidade de matriculados por Região',
           borderColor: '#ff6384',
-          data: dados.map(item => item.total)
+          data: dadosEspecificos.map(item => Number(item.total))
         }]
       }
     });
@@ -73,11 +146,11 @@ function atualizarGraficos(dados) {
     new Chart(document.getElementById('grafico-barra').getContext('2d'), {
       type: 'bar',
       data: {
-        labels: dados.map(item => item.regiao),
+        labels: dadosEspecificos.map(item => item.regiao),
         datasets: [{
           label: 'Quantidade de matriculados por Região',
           backgroundColor: ['#00BF63', '#FF914D', '#8D723D', '#A6A6A6', '#0047FF'],
-          data: dados.map(item => item.totalAlunos)
+          data: dadosEspecificos.map(item => Number(item.total))
         }]
       }
     });
@@ -86,11 +159,11 @@ function atualizarGraficos(dados) {
     new Chart(document.getElementById('grafico-horizontal-bar').getContext('2d'), {
       type: 'bar',
       data: {
-        labels: dados.map(item => item.regiao),
+        labels: dadosEspecificos.map(item => item.regiao),
         datasets: [{
           label: 'Quantidade de matriculados por Região',
           backgroundColor: ['#00BF63', '#FF914D', '#8D723D', '#A6A6A6', '#0047FF'],
-          data: dados.map(item => item.totalAlunos)
+          data: dadosEspecificos.map(item => Number(item.total))
         }]
       },
       options: {
